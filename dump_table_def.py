e74b49fc42a4407e05dee3e62a945cbacee9d981
Carl Kesselman <carl@isi.edu>
2018-06-21 22:33:55 -0700

    Added main function

--- dump_table_def.py
+++ dump_table_def.py
@@ -6,23 +6,6 @@ from deriva.core import HatracStore, ErmrestCatalog, ErmrestSnapshot, get_creden
 import deriva.core.ermrest_model as em
 import pprint
 
-parser = argparse.ArgumentParser(description='Generate a table configuration from an existing table.')
-parser.add_argument('--server', default='pbcconsortium.isrd.isi.edu',
-                    help='Catalog server name')
-parser.add_argument('table', help='schema:table_name)')
-
-args = parser.parse_args()
-
-server = args.server
-schema_name = args.table.split(':')[0]
-table_name = args.table.split(':')[1]
-
-credential = get_credential(server)
-catalog = ErmrestCatalog('https', server, 1, credentials=credential)
-model_root = catalog.getCatalogModel()
-schema = model_root.schemas[schema_name]
-table = schema.tables[table_name]
-
 def dump_table(table):
         tab = {
             'display' : table.display,
@@ -71,73 +54,110 @@ def dump_columns(table):
     ]
     return cols
 
-credential = get_credential(server)
-catalog = ErmrestCatalog('https', server, 1, credentials=credential)
-model_root = catalog.getCatalogModel()
-schema = model_root.schemas[schema_name]
-table = schema.tables[table_name]
-provide_system = False
-system_columns = ['RID','RCB','RMB','RCT','RMT']
 
-print('column_defs = [')
-for i in dump_columns(table):
-    if i['name'] in system_columns:
-        provide_system=True
-        continue
-    print('''    em.Column.define(
-    '{}',{},
-    nullok={},
-    annotations={}
-    comment={}),'''.format(i['name'],i['type'],i['nullok'],i['annotations'],
-               "'" + i['comment'] + "'" if i['comment'] != None else None))
 
-print('key_defs = [')
-for i in dump_keys(table):
-    print("""    em.Key.define(
-        {},
-        constraint_names={},
-        annotation={},
-        comment={}),""".format(i['key_columns'],i['names'],i['annotations'],
+def main():
+    parser = argparse.ArgumentParser(description='Dump annotations  for table {}:{}')
+    parser.add_argument('--server', default='pbcconsortium.isrd.isi.edu',
+                        help='Catalog server name')
+    parser.add_argument('table', help='schema:table_name)')
+    args = parser.parse_args()
 
+    server = args.server
+    schema_name = args.table.split(':')[0]
+    table_name = args.table.split(':')[1]
+
+    credential = get_credential(server)
+    catalog = ErmrestCatalog('https', server, 1, credentials=credential)
+    model_root = catalog.getCatalogModel()
+    schema = model_root.schemas[schema_name]
+    table = schema.tables[table_name]
+    print("""import argparse
+from deriva.core import ErmrestCatalog, get_credential, DerivaPathError
+import deriva.core.ermrest_model as em
+""")
+
+    provide_system = False
+    system_columns = ['RID', 'RCB', 'RMB', 'RCT', 'RMT']
+
+    print('column_defs = [')
+    for i in dump_columns(table):
+        if i['name'] in system_columns:
+            provide_system = True
+            continue
+        print('''    em.Column.define(
+        '{}',{},
+        nullok={},
+        annotations={}
+        comment={}),'''.format(i['name'], i['type'], i['nullok'], i['annotations'],
                                "'" + i['comment'] + "'" if i['comment'] != None else None))
-print(']')
 
-print('\nfkey_defs = [')
-for i in dump_foreign_keys(table):
-    print("""    em.ForeignKey.define(
-        {},
-        '{}', '{}', {},
-        constraint_names={},
-        annotations={},
-        acls={},
-        acl_bindings={},
-        on_update='{}', on_delete='{}',
-        comment={}),""".format(i['foreign_key_columns'], i['pk_schema'], i['pk_table'],i['pk_columns'],
-   i['constraint_names'], i['annotations'], i['acls'],i['acl_bindings'],i['on_update'], i['on_delete'], i['comment']))
-print(']')
+    print('key_defs = [')
+    for i in dump_keys(table):
+        print("""    em.Key.define(
+            {},
+            constraint_names={},
+            annotation={},
+            comment={}),""".format(i['key_columns'], i['names'], i['annotations'],
+                                   "'" + i['comment'] + "'" if i['comment'] != None else None))
+    print(']')
 
-print('table_annotations =')
-pprint.pprint(table.annotations, indent=4)
+    print('\nfkey_defs = [')
+    for i in dump_foreign_keys(table):
+        print("""    em.ForeignKey.define(
+            {},
+            '{}', '{}', {},
+            constraint_names={},
+            annotations={},
+            acls={},
+            acl_bindings={},
+            on_update='{}', on_delete='{}',
+            comment={}),""".format(i['foreign_key_columns'], i['pk_schema'], i['pk_table'], i['pk_columns'],
+                                   i['constraint_names'], i['annotations'], i['acls'], i['acl_bindings'],
+                                   i['on_update'], i['on_delete'], i['comment']))
+    print(']')
 
-print('''
-table_def = em.Table.define(
-  {},
-  column_defs,
-  key_defs=key_defs,
-  fkey_defs=fkey_defs,
-  comment={},
-  acls=acls,
-  acl_bindings=acl_bindings,
-  annotations=table_annotations,
-  provide_system={}
-)'''.format(table.name, table.comment, provide_system))
+    print('table_annotations =')
+    pprint.pprint(table.annotations, indent=4)
 
-print('''
-server = 'pbcconsortium.isrd.isi.edu'
-credential = get_credential(server)
-catalog = ErmrestCatalog('https', server, 1, credentials=credential)
-model_root = catalog.getCatalogModel()
-schema = model_root.schemas[schema_name]
-table = schema.create_table(catalog, table_def)
-'''
-)
+    print('''
+    table_def = em.Table.define(
+      {},
+      column_defs,
+      key_defs=key_defs,
+      fkey_defs=fkey_defs,
+      comment={},
+      acls=acls,
+      acl_bindings=acl_bindings,
+      annotations=table_annotations,
+      provide_system={}
+    )'''.format(table.name, table.comment, provide_system))
+
+    print("""
+def main():
+    parser = argparse.ArgumentParser(description='Load foreign key defs for table {0}:{1}')
+    parser.add_argument('--server', default='pbcconsortium.isrd.isi.edu',
+                        help='Catalog server name')
+    args = parser.parse_args()
+
+    server = args.server
+    schema_name = '{0}'
+    table_name = '{1}'
+
+    credential = get_credential(server)
+    catalog = ErmrestCatalog('https', server, 1, credentials=credential)
+    model_root = catalog.getCatalogModel()
+    schema = model_root.schemas[schema_name]
+    table = schema.tables[table_name]
+
+    table = schema.create_table(catalog, table_def)
+
+
+
+
+if __name__ == "__main__":
+    main()""".format(schema_name, table_name))
+
+
+if __name__ == "__main__":
+    main()
