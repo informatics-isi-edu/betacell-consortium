bdb83a6d20bfe08149fce7e92550b46c96643f4f
Carl Kesselman <carl@isi.edu>
2018-06-24 22:31:40 -0700

    More cleanup on def script.

--- dump_table_def.py
+++ dump_table_def.py
@@ -14,15 +14,30 @@ def print_annotations(table, stream):
         'visible_columns': table.visible_columns,
         'visible_foreign_keys': table.visible_foreign_keys,
         'table_display': table.table_display,
-        'table_annotations': table.annotations,
         'table_acls': table.acls,
         'table_acl_bindings': table.acl_bindings
     }
 
     for k, v in table_annotations.items():
-        if v != {}:
+        if v == {} or v == '':
+            print('{} = {}'.format(k, v), file=stream)
+        else:
             print('{} = \\'.format(k), file=stream)
             pprint.pprint(v, indent=4, width=80, depth=None, compact=False, stream=stream)
+            print('',file=stream)
+
+    print('table_annotations = {', file=stream)
+    for k,v in table.annotations.items():
+        if 'table-display' in k:
+            print('    "{}": table_display,'.format(k), file=stream)
+        elif 'visible-columns' in k:
+            print('    "{}" : visible_columns,'.format(k), file=stream)
+        elif 'visible-foreign-keys' in k:
+            print('    "{}" : visible_foreign_keys,'.format(k), file=stream)
+        else:
+            print('    "{}" :'.format(k), file=stream)
+            pprint.pprint(v, indent=8, stream=stream)
+    print('}', file=stream)
 
     column_annotations = {}
     column_acls = {}
@@ -50,10 +65,9 @@ def print_annotations(table, stream):
 
 
 def print_foreign_key_defs(table, stream):
-    print('\nfkey_defs = [', file=stream)
+    print('fkey_defs = [', file=stream)
     for fkey in table.foreign_keys:
-        print("""    em.ForeignKey.define(
-            {},
+        print("""    em.ForeignKey.define({},
             '{}', '{}', {},
             constraint_names = {},""".format([c['column_name'] for c in fkey.foreign_key_columns],
                                              fkey.referenced_columns[0]['schema_name'],
@@ -74,8 +88,7 @@ def print_foreign_key_defs(table, stream):
 def print_key_defs(table, stream):
     print('key_defs = [', file=stream)
     for key in table.keys:
-        print("""    em.Key.define(
-                   {},
+        print("""    em.Key.define({},
                    constraint_names={},""".format(key.unique_columns, key.names), file=stream)
         for i in ['annotations',  'comment']:
             a = getattr(key, i)
@@ -95,8 +108,7 @@ def print_column_defs(table, stream):
         if col.name in system_columns:
             provide_system = True
             continue
-        print('''    em.Column.define(
-        '{}', em.builtin_types['{}'],
+        print('''    em.Column.define('{}', em.builtin_types['{}'],
         nullok = {},'''.format(col.name,
                         col.type.typename + '[]' if 'is_array' is True else col.type.typename,
                         col.nullok,), file=stream)
@@ -111,24 +123,16 @@ def print_column_defs(table, stream):
 
 
 def print_table_def(table, provide_system, stream):
-    print("""
-table_def = em.Table.define(
-    '{}',""".format(table.name), file=stream)
-    for i in ['key_defs', 'fkey_defs', 'annotations', 'acls', 'acl_bindings', 'comment', ]:
-            a = getattr(table, i)
-            if not (a == {} or a is None or a == 'NO ACTION' or a == ''):
-                v = "'" + a + "'" if re.match('comment|on_update|on_delete', i) else a
-                print("        {} = {},".format(i, v), file=stream)
-        print('    ),', file=stream)
+    print("""table_def = em.Table.define('{}',
     column_defs=column_defs,
     key_defs=key_defs,
     fkey_defs=fkey_defs,
     annotations=table_annotations,
     acls=table_acls,
-    acl_bindings=acl_bindings,
-    comment=comment,
+    acl_bindings=table_acl_bindings,
+    comment='{}',
     provide_system = {}
-)""".format(table.name, provide_system), file=stream)
+)""".format(table.name, table.comment, provide_system), file=stream)
 
 
 def print_defs(server, schema_name, table_name, stream):
--- load_catalog.py
+++ load_catalog.py
@@ -8,27 +8,22 @@ def main():
     parser.add_argument('--server', default='pbcconsortium.isrd.isi.edu',
                         help='Catalog server name')
     parser.add_argument('--delete', help='delete existing foreign keys before loading)')
-    parser.add_argument('file', help='Name of file with table definitions.')
-    parser.add_argument('mode', help='Operation to perform: table|annotations|fkeys')
-
-    create_table = load_fkeys = load_annotations = load_acls = False
+    parser.add_argument('--defpath', default='.', help='path to table definitions)')
+    parser.add_argument('table', help='Name table to be loaded.')
+    parser.add_argument('mode', choices=['table', 'annotations', 'fkeys', 'acls'],
+                        help='Operation to perform')
 
     args = parser.parse_args()
     server = args.server
-    if args.mode == 'table':
-        create_table = True
-    elif args.mode == 'annotations':
-        load_annotations = True
-    elif args.mode == 'fkeys':
-        load_fkeys = True
-    elif args.mode == 'acls':
-        load_acls = True
-    else:
-        print("Invalad mode")
-        return
+    mode = args.mode
+    table = args.table
+    defpath = args.defpath
 
-    print('Importing ', args.file)
-    mod = importlib.import_module(args.file)
+    print('Importing ', table)
+
+    module_spec = importlib.util.spec_from_file_location(table , '{}/{}_def.py'.format(defpath, table))
+    mod = importlib.util.module_from_spec(module_spec)
+    module_spec.loader.exec_module(mod)
 
     credential = get_credential(server)
     catalog = ErmrestCatalog('https', server, 1, credentials=credential)
@@ -38,18 +33,17 @@ def main():
 
     skip_fkeys = False
 
-    if create_table:
+    if mode == 'table':
         if skip_fkeys:
             mod.table_def.fkey_defs = []
-        print(mod.table_def)
-  #      table = schema.create_table(catalog, mod.table_def)
-    if load_fkeys:
+        table = schema.create_table(catalog, mod.table_def)
+    if mode == 'fkeys':
             for k in table.foreign_keys:
                 k.delete(catalog)
             for i in mod.fkey_defs:
                 table.create_fkey(catalog, i)
 
-    if load_annotations:
+    if mode == 'annotations':
         if len(mod.visible_columns) > 0:
             for k, v in mod.visible_columns.items():
                 table.visible_columns[k] = v
@@ -60,7 +54,7 @@ def main():
         table.annotations['table_display'] = mod.table_display
         table.apply(catalog)
 
-    if load_acls:
+    if mode == 'acls':
         if len(mod.acls > 0):
             for k, v in mod.acls.items():
                 table.acls[k] = v
