ab4edb67c2986e86442e25f79ff870c64f7d5af7
Carl Kesselman <carl@isi.edu>
2018-09-14 08:28:24 -0700

    More work on lint.

--- lint.py
+++ lint.py
@@ -12,36 +12,66 @@ tag_names = {
     'tag:isrd.isi.edu,2016:export',
 }
 
+aggregate_names = ['max', 'min', 'array', 'array_d', 'cnt', 'cnt_d']:
+
+def validate_pseudocolumn(spec, context):
+    for k, v in spec.items():
+        if k is "entity" and (v is True or v is False):
+            continue
+        if (k == "markdown_name" or k == 'comment') and type(v) is str:
+            continue
+        if k == 'source' and type(v) is list:
+            continue
+        if k == 'aggregate' and v in aggregate_names:
+            continue
+        print('Invalid pseudocolumn spec {} {}:{}'.format(context, k, v))
+
+
 def lint_path(fk_path, model, table, context):
+    path = ''
     for fk in fk_path:
+        # Get list of column names.
         cnames = [i.name for i in table.column_definitions]
+
+        # Construct a map of constraint names to target tables.
         out_fkeys = {i.names[0][0] + ':' + i.names[0][1]:
                      model.schemas[i.referenced_columns[0]['schema_name']].tables[i.referenced_columns[0]['table_name']]
             for i in table.foreign_keys}
         in_fkeys = {i.names[0][0] + ":" + i.names[0][1]:
-                        model.schemas[i.referenced_columns[0]['schema_name']].tables[i.referenced_columns[0]['table_name']]
+                        model.schemas[i.sname].tables[i.tname]
             for i in table.referenced_by}
+
         if type(fk) == str:
             # Column name
+            path = path + '/' + fk
             if fk not in cnames:
-                print('Invalid  column name {} {}'.format(context, fk))
+                print('Invalid  column name {} {}'.format(context, path))
+        elif type(fk) is list:
+            # Old style column name [schema, fkey]
+            k = '{}:{}'.format(*fk)
+            path = path + '/' + k
+            if not (k in out_fkeys or k in in_fkeys):
+                print('Bad columm spec {} {}'.format(context, path))
         elif 'inbound' in fk:
             k = '{}:{}'.format(*fk['inbound'])
+            path = path + '/' + k
             if not k in in_fkeys:
-                print('missing in path fkey {} {}'.format(context, fk))
+                print('Bad column spec: {} {}'.format(context, path))
                 return
             else:
                 table = in_fkeys[k]
         elif 'outbound' in fk:
             k = '{}:{}'.format(*fk['outbound'])
+            path = path + '/' + k
             if not k in out_fkeys:
-                print('missing out path fkey {} {}'.format(context, fk))
+                print('Bad column spec: {} {}'.format(context, path))
                 return
             else:
                 table = out_fkeys[k]
+        elif fk in ['choices', 'ranges', 'search', 'not_null']:
+            pass
         else:
-            print('Bad path element {}'.format(fk))
-
+            print('{}: Bad path element {}'.format(context, path))
 
 def lint_columns( column_list, model, table, context):
     cnames = [i.name for i in table.column_definitions]
@@ -51,12 +81,12 @@ def lint_columns( column_list, model, table, context):
         if type(i) == str:
             # Column name
             if i not in cnames:
-                print('Invalid column name {} {}'.format(context, i))
+                print('{}: Invalid column name {}'.format(context, i))
         elif type(i) is list:
             # Old style column name [schema, fkey]
-            if not (tuple(i) in out_fkeys or tuple(i) in in_fkeys):
-                print('missing fkey {} {}'.format(context, i))
+            lint_path([i], model, table, context)
         elif type(i) is dict:
+            validate_pseudocolumn(i, context)
             lint_path( i['source'], model, table, context)
         else:
             print('Unknown path {}'.format(i))
@@ -67,13 +97,22 @@ def lint_table(catalog, table):
     # Check to see if all the annotations are valid
     for i in table.annotations.keys():
         if i not in tag_names:
-            print("Invalid tag {}".format(i))
+            print("Invalid tag: {}".format(i))
 
     for f, a in table.visible_columns.items():
         if f not in ['*', 'filter', 'compact', 'detailed', 'entry']:
-            print('Invalid context {}'.format(f))
+            print('Invalid context: {}'.format(f))
             continue
         if f == 'filter':
             lint_columns( a['and'], model_root, table, 'visible_column[{}]'.format(f))
         else:
             lint_columns( a, model_root, table, 'visible_column[{}]'.format(f))
+
+    for f, a in table.visible_foreign_keys.items():
+        if f not in ['*', 'compact', 'detailed', 'entry']:
+            print('Invalid context {}'.format(f))
+            continue
+        if f == 'filter':
+            lint_columns( a['and'], model_root, table, 'visible_foreign_key[{}]'.format(f))
+        else:
+            lint_columns( a, model_root, table, 'visible_foreign_keys[{}]'.format(f))
